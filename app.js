31e20edca35a45d974285b70de666e95377ae0ca
XploR5 <brohan5501@gmail.com>
2023-01-08 01:48:41 +0530

    Improved basic functionality

--- app.js
+++ app.js
@@ -1,159 +1,153 @@
-console.log('Welcome to calculator app!')
+console.log('Welcome to the calculator APP!')
 
-function zoom() {
-  document.body.style.zoom = '100%'
+function fac(n) {
+  if (n < 0) {
+    return -1
+  } else if (n == 0) {
+    return 1
+  } else {
+    return n * fac(n - 1)
+  }
+}
+
+function Sq(n) {
+  return n * n
+}
+
+function Cu(n) {
+  return n * n * n
+}
+
+function sqrt(n) {
+  return Math.sqrt(n)
+}
+
+function sin(n) {
+  return Math.sin(n)
+}
+
+function cos(n) {
+  return Math.cos(n)
+}
+
+function tan(n) {
+  return Math.tan(n)
+}
+
+function log10(n) {
+  return Math.log10(n)
+}
+
+function log(n) {
+  return Math.log(n)
+}
+
+function pow(n, m) {
+  return Math.pow(n, m)
 }
 
 class Calculator {
-  constructor(preCalcTextElement, curCalcTextElement) {
-    this.preCalcTextElement = preCalcTextElement
-    this.curCalcTextElement = curCalcTextElement
+  constructor(preCalculation, displayAnswer) {
+    this.preCalculation = preCalculation
+    this.displayAnswer = displayAnswer
     this.clear()
   }
 
-  clear() {
-    this.preCalc = ''
-    this.curCalc = ''
-    this.operation = undefined
-    this.updateDisplay()
+  updateNumber(input) {
+    this.preCalculation.innerText = this.preCalculation.innerText + input
   }
 
-  delete() {
-    this.curCalc = this.curCalc.toString().slice(0, -1)
-  }
-
-  appendNumber(number) {
-    if (number === '.' && this.curCalc.includes('.')) return
-    this.curCalc = this.curCalc.toString() + number.toString()
-  }
-
-  chooseOperation(operation, type) {
-    if (this.curCalc === '' && type === 'normal') return
-    if (this.preCalc !== '') {
-      this.compute(type)
-    }
-    this.operation = operation
-    this.preCalc = this.curCalc.toString() + ' ' + this.operation.toString()
-    this.curCalc = ''
-  }
-
-  compute(type) {
-    let computation = 0
-    const prev = parseFloat(this.preCalc)
-    const current = parseFloat(this.curCalc)
-
-    const factorial = (n) => {
-      if (n === 0 || n === 1) return 1
-      for (let i = n - 1; i >= 1; i--) {
-        n *= i
-      }
-      return n
-    }
-
-    // if (isNaN(prev) && type !== 'preOperation') return
-    if (isNaN(current)) {
-      if (this.operation === '!') {
-        this.curCalc = factorial(prev)
-        return
-      }
-      this.curCalc = prev
-      this.operation = undefined
-      this.preCalc = ''
-      return
-    }
-
-    switch (this.operation) {
-      case '➕':
-        computation = prev + current
-        break
-      case '➖':
-        computation = prev - current
-        break
-      case '✳︎':
-        computation = prev * current
-        break
-      case '➗':
-        computation = prev / current
-        break
-      case '^':
-        computation = Math.pow(prev, current)
-        break
-      case '!':
-        this.curCalc = factorial(prev)
-        break
-      case '√':
-        computation = Math.sqrt(current)
-        break
+  updateOperation(input) {
+    switch (input) {
       case 'sin':
-        computation = Math.sin(current)
+        this.preCalculation.innerText += 'sin('
         break
       case 'cos':
-        computation = Math.cos(current)
+        this.preCalculation.innerText += 'cos('
         break
       case 'tan':
-        computation = Math.tan(current)
+        this.preCalculation.innerText += 'tan('
         break
       case 'log':
-        computation = Math.log10(current)
+        this.preCalculation.innerText += 'log10('
         break
+      case 'ln':
+        this.preCalculation.innerText += 'log('
+        break
+      case '√':
+        this.preCalculation.innerText += 'sqrt('
+        break
+      case 'π':
+        this.preCalculation.innerText += '3.1415'
+        break
+      case 'e':
+        this.preCalculation.innerText += '2.7182'
+        break
+      case 'Sq':
+        this.preCalculation.innerText += 'Sq('
+        break
+      case 'Cu':
+        this.preCalculation.innerText += 'Cu('
+        break
+      case 'pow':
+        this.preCalculation.innerText += 'pow('
+        break
+      case '!':
+        this.preCalculation.innerText += 'fac('
+        break
+
       default:
         return
     }
-    this.curCalc = computation.toPrecision(5)
-    this.operation = undefined
-    this.preCalc = ''
   }
 
-  updateDisplay() {
-    this.curCalcTextElement.innerText = this.curCalc
-    this.preCalcTextElement.innerText = this.preCalc
+  clear() {
+    this.preCalculation.innerText = ''
+    this.displayAnswer.innerText = ''
+  }
+
+  delete() {
+    this.preCalculation.innerText = this.preCalculation.innerText.slice(0, -1)
+  }
+
+  calculate() {
+    displayAnswer.innerText = eval(preCalculation.innerText)
   }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
-const specialNumberButtons = document.querySelectorAll('[data-number-special]')
-
+const simpleOperationButtons = document.querySelectorAll(
+  '[data-operation-simple]'
+)
 const operationButtons = document.querySelectorAll('[data-operation]')
-const preOperationButtons = document.querySelectorAll('[data-pre-operation]')
 
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 
-const preCalcTextElement = document.querySelector('[data-pre-calc]')
-const curCalcTextElement = document.querySelector('[data-cur-calc]')
+const preCalculation = document.querySelector('[data-calculation]')
+const displayAnswer = document.querySelector('[data-answer]')
 
-const calculator = new Calculator(preCalcTextElement, curCalcTextElement)
+preCalculation.innerText = 'preCalculation'
+displayAnswer.innerText = 'displayAnswer'
+
+const calculator = new Calculator(preCalculation, displayAnswer)
 
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    calculator.appendNumber(button.innerText)
-    calculator.updateDisplay()
+    calculator.updateNumber(button.innerText)
   })
 })
 
-specialNumberButtons.forEach((button) => {
+simpleOperationButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    calculator.appendNumber(
-      button.innerText === 'π'
-        ? Math.PI.toPrecision(10)
-        : Math.E.toPrecision(10)
-    )
-    calculator.updateDisplay()
+    calculator.updateNumber(button.innerText)
   })
 })
 
 operationButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    calculator.chooseOperation(button.innerText, 'normal')
-    calculator.updateDisplay()
-  })
-})
-
-preOperationButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    calculator.chooseOperation(button.innerText, 'preOperation')
-    calculator.updateDisplay()
+    calculator.updateOperation(button.innerText)
   })
 })
 
@@ -163,10 +157,140 @@ allClearButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', () => {
   calculator.delete()
-  calculator.updateDisplay()
 })
 
 equalsButton.addEventListener('click', () => {
-  calculator.compute()
-  calculator.updateDisplay()
+  calculator.calculate()
 })
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'Enter':
+      calculator.calculate()
+      break
+    case '0':
+      calculator.updateNumber('0')
+      break
+    case '1':
+      calculator.updateNumber('1')
+      break
+    case '2':
+      calculator.updateNumber('2')
+      break
+    case '3':
+      calculator.updateNumber('3')
+      break
+    case '4':
+      calculator.updateNumber('4')
+      break
+    case '5':
+      calculator.updateNumber('5')
+      break
+    case '6':
+      calculator.updateNumber('6')
+      break
+    case '7':
+      calculator.updateNumber('7')
+      break
+    case '8':
+      calculator.updateNumber('8')
+      break
+    case '9':
+      calculator.updateNumber('9')
+      break
+    case '+':
+      calculator.updateNumber('+')
+      break
+    case '-':
+      calculator.updateNumber('-')
+      break
+    case '*':
+      calculator.updateNumber('*')
+      break
+    case '/':
+      calculator.updateNumber('/')
+      break
+    case '.':
+      calculator.updateNumber('.')
+      break
+    case '(':
+      calculator.updateNumber('(')
+      break
+    case ')':
+      calculator.updateNumber(')')
+      break
+    case 'Backspace':
+      calculator.delete()
+      break
+    case 'Delete':
+      calculator.clear()
+      break
+    case 'Enter':
+      calculator.calculate()
+      break
+    case 's':
+      calculator.updateOperation('sin')
+      break
+    case 'c':
+      calculator.updateOperation('cos')
+      break
+    case 't':
+      calculator.updateOperation('tan')
+      break
+    case 'l':
+      calculator.updateOperation('log')
+      break
+    case 'L':
+      calculator.updateOperation('ln')
+      break
+    case 'S':
+      calculator.updateOperation('Sq')
+      break
+    case 'C':
+      calculator.updateOperation('Cu')
+      break
+    case 'p':
+      calculator.updateOperation('pow')
+      break
+    case 'f':
+      calculator.updateOperation('!')
+      break
+    case 'm':
+      calculator.updateNumber('%')
+      break
+    case 'r':
+      calculator.updateOperation('√')
+      break
+    case 'i':
+      calculator.updateOperation('π')
+      break
+    case 'e':
+      calculator.updateOperation('e')
+      break
+    default:
+      return
+  }
+})
+
+var buttons = document.getElementsByTagName('button')
+
+Array.prototype.forEach.call(buttons, function (b) {
+  b.addEventListener('click', createRipple)
+})
+
+function createRipple(e) {
+  if (this.getElementsByClassName('ripple').length > 0) {
+    this.removeChild(this.childNodes[1])
+  }
+
+  var circle = document.createElement('div')
+  this.appendChild(circle)
+
+  var d = Math.max(this.clientWidth, this.clientHeight)
+  circle.style.width = circle.style.height = d + 'px'
+
+  circle.style.left = e.clientX - this.offsetLeft - d / 2 + 'px'
+  circle.style.top = e.clientY - this.offsetTop - d / 2 + 'px'
+
+  circle.classList.add('ripple')
+}
